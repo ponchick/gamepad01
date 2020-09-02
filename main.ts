6382c51d12429c4484b0bd0d0d6cfb5a10845991
ponchick <903900+ponchick@users.noreply.github.com>
2020-09-02 19:06:47 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,103 @@
 gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Up, function () {
-    radio.sendNumber(0)
+    Вправо = false
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Up, function () {
-    radio.sendNumber(0)
+    Влево = false
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Down, function () {
-    radio.sendNumber(3)
+    Влево = true
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P2, GamerBitEvent.Up, function () {
-    radio.sendNumber(0)
+    Вниз = false
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Down, function () {
-    radio.sendNumber(4)
+    Вправо = true
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Down, function () {
-    radio.sendNumber(2)
+    Назад = true
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P8, GamerBitEvent.Up, function () {
-    radio.sendNumber(0)
+    Вперед = false
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P1, GamerBitEvent.Up, function () {
-    radio.sendNumber(0)
+    Вверх = false
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P8, GamerBitEvent.Down, function () {
-    radio.sendNumber(1)
+    Вперед = true
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P1, GamerBitEvent.Down, function () {
-    radio.sendNumber(5)
+    Вверх = true
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Up, function () {
-    radio.sendNumber(0)
+    Назад = false
+    Изменение = true
 })
 gamePad.onEvent(GamerBitPin.P2, GamerBitEvent.Down, function () {
-    radio.sendNumber(6)
+    Вниз = true
+    Изменение = true
 })
+let Вверх = false
+let Вперед = false
+let Назад = false
+let Вниз = false
+let Влево = false
+let Вправо = false
+let Изменение = false
+Изменение = false
 radio.setGroup(1)
+basic.forever(function () {
+    if (Изменение) {
+        if (Вперед) {
+            if (Влево) {
+                radio.sendNumber(7)
+            } else if (Назад) {
+                radio.sendNumber(0)
+            } else if (Вправо) {
+                radio.sendNumber(9)
+            } else {
+                radio.sendNumber(8)
+            }
+        } else if (Назад) {
+            if (Влево) {
+                radio.sendNumber(1)
+            } else if (Вправо) {
+                radio.sendNumber(3)
+            } else {
+                radio.sendNumber(2)
+            }
+        } else if (Влево) {
+            if (Вправо) {
+                radio.sendNumber(0)
+            } else {
+                radio.sendNumber(4)
+            }
+        } else if (Вправо) {
+            radio.sendNumber(6)
+        } else {
+            radio.sendNumber(0)
+        }
+        if (Вверх) {
+            if (Вниз) {
+                radio.sendNumber(10)
+            } else {
+                radio.sendNumber(18)
+            }
+        } else if (Вниз) {
+            radio.sendNumber(12)
+        } else {
+            radio.sendNumber(10)
+        }
+        Изменение = false
+    }
+    basic.pause(100)
+})
